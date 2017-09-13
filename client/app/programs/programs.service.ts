2c20be62eefb282091cb1a3436d9971a5c84c595
Ben D'Herville <ben.dherville@encompasscorporation.com>
2017-09-13 19:27:03 +1000

    Use relative urls

--- client/app/programs/programs.service.ts
+++ client/app/programs/programs.service.ts
@@ -12,7 +12,7 @@ export class ProgramsService {
 
     findPrograms() {
         return new Promise(resolve => {
-            this.http.get('http://localhost:3000/programs')
+            this.http.get('/programs')
                 .subscribe(programs => {
                     resolve(programs.json());
                 });
@@ -21,7 +21,7 @@ export class ProgramsService {
 
     getLatestPlaylist(programId) {
         return new Promise(resolve => {
-            this.http.get(`http://localhost:3000/programs/${programId}/latestplaylist`)
+            this.http.get(`/programs/${programId}/latestplaylist`)
                 .subscribe(playlist => {
                     resolve(playlist.json());
                 });
